024b81e13d04fb8a1bfd395924e50ae2ed0818ea
wasimfaris2 <85233139+wasimfaris2@users.noreply.github.com>
2021-06-25 11:19:30 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -17,6 +17,10 @@ from tg_bot.modules import ALL_MODULES
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
+NESRI_IMG = "https://telegra.ph/file/5ced61d0da061dd952566.jpg"
+
+
+
 PM_START_TEXT = """
 
 *ഹായ് {}, എന്റെ പേര് {}!*
